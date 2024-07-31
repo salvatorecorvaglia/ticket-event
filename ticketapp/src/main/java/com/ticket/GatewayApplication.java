9e66042f8b9b5eacba16c1bb9a0cdf59c71270c4
Salvatore Corvaglia <ingegnere.corvaglia@gmail.com>
2024-07-31 23:42:01 +0200

    Fourth commit

--- ticketapp/src/main/java/com/ticket/GatewayApplication.java
+++ ticketapp/src/main/java/com/ticket/GatewayApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TicketAppApplication {
+public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TicketAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 }
